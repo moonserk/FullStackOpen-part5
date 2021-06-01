c300d5b3872fde557e4d750566d38fd016050375
Gregory Romantsov <imoonserk@gmail.com>
2021-06-01 20:28:50 +0400

    5.2: bloglist frontend, step2

--- src/App.js
+++ src/App.js
@@ -18,14 +18,24 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogsappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
-    console.log(username, password)
 
     try{
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem('loggedBlogsappUser', JSON.stringify(user))
+
       setUser(user)
       setUsername('')
       setPassword('')
@@ -48,9 +58,16 @@ const App = () => {
     </div>
   )
 
+  const logoutHandler = () => {
+    setUser(null)
+    window.localStorage.clear()
+  }
+
   const blogsList = () => (
     <div>
-      <p>{user.name} logged-in</p>
+    <p>{user.name} logged-in
+      <button onClick={logoutHandler}>logout</button>
+    </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
